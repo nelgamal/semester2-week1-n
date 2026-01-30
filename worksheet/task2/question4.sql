d13e10dd12eaf3d541b8838a6bf855f36eb02713
nelgamal <znhg0906@leeds.ac.uk>
2026-01-30 18:00:57 +0000

    End of Questions.

--- worksheet/task2/question4.sql
+++ worksheet/task2/question4.sql
@@ -1,3 +1,13 @@
 -- List all students along with the names of the courses they are enrolled in.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, CourseName
+SELECT
+    Student.StudentId,
+    FirstName,
+    LastName, 
+    CourseName 
+FROM Student
+JOIN Enrolment
+    ON Enrolment.StudentId = Student.StudentId
+JOIN Course
+    ON Course.CourseId = Enrolment.CourseId;
